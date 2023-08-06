52d19419c148aa949f51830cff75e326ae008f17
moepig <uncho.tnct@gmail.com>
2023-08-06 15:22:08 +0900

    ユニットテスト時にも Lombok を使いたい

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testCompileOnly ("org.projectlombok:lombok")
+	testAnnotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
